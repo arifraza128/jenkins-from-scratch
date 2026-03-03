2e9673c079a79ee0071b78dad9c9f22db7161127
Arif Raza <arif3raza@gmail.com>
2026-03-03 17:42:43 +0530

    Create variables.groovy

---
+++ groovysyntax/variables.groovy
@@ -0,0 +1,19 @@
+def name = "Arif"
+def age = 21
+println(name)
+println(age)
+
+#String Interpolation
+def name = "Arif"
+def age = 21
+
+println("My name is $name and I am $age years old")
+
+
+#integer Variables
+
+def marks = 90
+def semester = 6
+
+println(marks)
+println(semester)
