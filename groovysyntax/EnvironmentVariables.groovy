3bc3cbd9692ebde6ae02a6ab8f84e4a78b805c66
Arif Raza <arif3raza@gmail.com>
2026-05-09 20:07:20 +0530

    Update EnvironmentVariables.groovy

--- groovysyntax/EnvironmentVariables.groovy
+++ groovysyntax/EnvironmentVariables.groovy
@@ -1,1 +1,16 @@
+pipeline {
+    agent any
 
+    environment {
+        APP_NAME = "MyApp"
+        VERSION = "1.0.0"
+    }
+
+    stages {
+        stage('Info') {
+            steps {
+                echo "Deploying ${APP_NAME} version ${VERSION}"
+            }
+        }
+    }
+}
