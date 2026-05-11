b4dd5bc586c030301ea6402f0e44db0c6fdf514c
Arif Raza <arif3raza@gmail.com>
2026-05-11 21:59:46 +0530

    Add dynamic stage creation for services in pipeline

--- groovysyntax/DynamicStageCreation.groovy
+++ groovysyntax/DynamicStageCreation.groovy
@@ -1,1 +1,17 @@
+def services = ['auth', 'payment', 'orders']
 
+pipeline {
+    agent any
+
+    stages {
+        stage('Dynamic Stages') {
+            steps {
+                script {
+                    services.each { service ->
+                        echo "Building ${service}"
+                    }
+                }
+            }
+        }
+    }
+}
