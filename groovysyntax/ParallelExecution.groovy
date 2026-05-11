90550ff0ea21a4e0d7ba6ac81c726cc637ae0a4e
Arif Raza <arif3raza@gmail.com>
2026-05-11 22:02:23 +0530

    Add parallel execution for frontend and backend stages

--- groovysyntax/ParallelExecution.groovy
+++ groovysyntax/ParallelExecution.groovy
@@ -1,1 +1,21 @@
+pipeline {
+    agent any
 
+    stages {
+        stage('Parallel Jobs') {
+            parallel {
+                stage('Frontend') {
+                    steps {
+                        echo 'Building frontend'
+                    }
+                }
+
+                stage('Backend') {
+                    steps {
+                        echo 'Building backend'
+                    }
+                }
+            }
+        }
+    }
+}
